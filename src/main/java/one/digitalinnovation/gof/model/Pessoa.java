03d1b4143a1cbd26e4cef3e279508249e7008514
PedroVictorAndreoli <114033109+PedroVictorAndreoli@users.noreply.github.com>
2024-07-23 22:04:52 -0300

    Update Pessoa.java

--- src/main/java/one/digitalinnovation/gof/model/Pessoa.java
+++ src/main/java/one/digitalinnovation/gof/model/Pessoa.java
@@ -6,7 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-
+@MappedSuperclass
 public abstract class Pessoa {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
